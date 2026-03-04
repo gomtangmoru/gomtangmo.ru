ed134f8988325900eb59a77e6e94531be537d4ee
JaeYoung Ryu <admin@gomtangmo.ru>
2026-03-05 04:13:27 +0900

    feat: add robot.txt

--- bps/routes.py
+++ bps/routes.py
@@ -6,3 +6,8 @@ blueprint = Blueprint('root', __name__)
 def index():
     return render_template('index.htm'), 200
 
+@blueprint.route('/robot.txt')
+def robot():
+    return send_from_directory(current_app.static_folder, 'robot.txt'), 200
+
+

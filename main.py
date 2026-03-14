7d2e533d13652a4c862916cad03e1b8c823ab7a4
JaeYoung Ryu <admin@gomtangmo.ru>
2026-03-15 02:16:35 +0900

    fix broken page by defining mimetypes

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from bps.routes import blueprint
+import mimetypes
+
+mimetypes.add_type('application/javascript', '.js')
+mimetypes.add_type('text/css', '.css')
 
 app = Flask(__name__, static_folder='statics')
 app.register_blueprint(blueprint)

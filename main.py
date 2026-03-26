c23bc087658147a05614372328ac9bb253f9af87
JaeYoung Ryu <admin@gomtangmo.ru>
2026-03-27 02:30:45 +0900

    feat: update image references, add CI/CD skill tag, add s.thanks tag

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from flask import Flask
 from bps.routes import blueprint
 import mimetypes
 
+
 mimetypes.add_type('application/javascript', '.js')
 mimetypes.add_type('text/css', '.css')
 
 app = Flask(__name__, static_folder='statics')
 app.register_blueprint(blueprint)
 
-if __name__ == '__main__':
+
+def main() -> None:
     app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
+
